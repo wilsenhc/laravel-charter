c5cf68e070d11c8cdbe7e502ba3c600584fab317
Wilsen Hernández <wilsenh95@gmail.com>
2026-07-03 10:17:31 -0400

    feat: add dynamic sitemap and github link

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,29 @@
 
 use App\Http\Controllers\BuildController;
 use Illuminate\Support\Facades\Route;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 Route::get('/', [BuildController::class, 'index'])->name('build.index');
 
 Route::get('/build', [BuildController::class, 'show'])->name('build.show');
+
+Route::get('/sitemap.xml', function () {
+    $sitemap = Sitemap::create();
+
+    $sitemap->add(
+        Url::create(url('/'))
+            ->setLastModificationDate(now())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(1.0),
+    );
+
+    $sitemap->add(
+        Url::create(url('/build'))
+            ->setLastModificationDate(now())
+            ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
+            ->setPriority(0.9),
+    );
+
+    return response($sitemap->render(), 200, ['Content-Type' => 'application/xml']);
+})->name('sitemap');
