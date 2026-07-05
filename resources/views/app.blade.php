78078f5f8a81c587def4acdfc835e84ab648ffcf
Wilsen Hernández <wilsenh95@gmail.com>
2026-07-05 16:17:08 -0400

    chore: remove temporary favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
         <script>
             (function () {
                 const stored = localStorage.getItem('vueuse-color-scheme') || 'auto';
