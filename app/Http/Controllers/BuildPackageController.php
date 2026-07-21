a80e8c0640c1be8d3bdc494071861e9020495039
Wilsen Hernández <wilsenh95@gmail.com>
2026-07-21 19:50:34 -0400

    feat: refactor build endpoints logic to actions

--- app/Http/Controllers/BuildPackageController.php
+++ app/Http/Controllers/BuildPackageController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\BuildPackageScript;
 use App\Http\Requests\BuildPackageRequest;
 use App\Jobs\RecordPackageBuildStat;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
 use Inertia\Response as InertiaResponse;
@@ -20,66 +20,21 @@ class BuildPackageController extends Controller
         ]);
     }
 
-    public function show(BuildPackageRequest $request): Response
+    public function show(BuildPackageRequest $request, BuildPackageScript $buildScript): Response
     {
-        $name = $request->validated('name');
-        $php = $request->validated('php');
-        $features = $request->validated('features', []);
+        $validated = $request->validated();
 
-        $featureFlags = array_map(fn (string $feature) => "--{$feature}", $features);
-
-        $metadataFlags = [];
-
-        if ($authorName = $request->validated('author_name')) {
-            $metadataFlags[] = '--author-name=\\"'.$authorName.'\\"';
-        }
-
-        if ($authorEmail = $request->validated('author_email')) {
-            $metadataFlags[] = '--author-email=\\"'.$authorEmail.'\\"';
-        }
-
-        if ($packageName = $request->validated('package_name')) {
-            $metadataFlags[] = '--package-name=\\"'.$packageName.'\\"';
-        }
-
-        if ($packageNameHuman = $request->validated('package_name_human')) {
-            $metadataFlags[] = '--package-name-human=\\"'.$packageNameHuman.'\\"';
-        }
-
-        if ($packageDescription = $request->validated('package_description')) {
-            $metadataFlags[] = '--package-description=\\"'.$packageDescription.'\\"';
-        }
-
-        if ($vendorNamespace = $request->validated('vendor_namespace')) {
-            $metadataFlags[] = '--vendor-namespace=\\"'.$vendorNamespace.'\\"';
-        }
-
-        if ($className = $request->validated('class_name')) {
-            $metadataFlags[] = '--class-name=\\"'.$className.'\\"';
-        }
-
-        $options = implode(' ', [
-            ...$featureFlags,
-            ...$metadataFlags,
-        ]);
-
-        $script = Blade::render(
-            (string) file_get_contents(resource_path('stubs/build-package.sh')),
-            [
-                'name' => $name,
-                'options' => $options,
-                'php' => $php,
-            ],
-        );
+        $script = $buildScript->handle($validated);
 
         if (! Str::contains($request->userAgent() ?? '', 'Mozilla')) {
+            $features = $request->validated('features', []);
             $featureData = array_combine(
                 $features,
                 array_fill(0, count($features), true),
             );
 
             RecordPackageBuildStat::dispatch([
-                'php_version' => $php,
+                'php_version' => $validated['php'],
                 'config' => $featureData['config'] ?? false,
                 'routes' => $featureData['routes'] ?? false,
                 'views' => $featureData['views'] ?? false,
