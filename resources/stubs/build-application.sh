d25e91a07d427f4e596116ee7f610ccdc8b2c1fb
Wilsen Hernández <wilsenh95@gmail.com>
2026-08-14 10:02:04 -0400

    fix: fix logo x2

--- resources/stubs/build-application.sh
+++ resources/stubs/build-application.sh
@@ -4,11 +4,11 @@ BOLD='\033[1m'
 NC='\033[0m'
 
 echo ""
-echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____  ${NC}"
-echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \ ${NC}"
-echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) |${NC}"
-echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ < ${NC}"
-echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\\${NC}"
+echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____   ${NC}"
+echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \  ${NC}"
+echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) | ${NC}"
+echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ <  ${NC}"
+echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\ ${NC}"
 echo ""
 
 docker info > /dev/null 2>&1
@@ -90,11 +90,11 @@ else
 fi
 
 echo ""
-echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____  ${NC}"
-echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \ ${NC}"
-echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) |${NC}"
-echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ < ${NC}"
-echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\\${NC}"
+echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____   ${NC}"
+echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \  ${NC}"
+echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) | ${NC}"
+echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ <  ${NC}"
+echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\ ${NC}"
 echo ""
 echo -e "${CYAN}Enjoying Charter for Laravel? Consider supporting development:${NC}"
 echo -e "${BOLD}https://paypal.me/wilsenjhc${NC}"
--- resources/stubs/error.sh
+++ resources/stubs/error.sh
@@ -7,11 +7,11 @@ LIGHT_CYAN='\033[1;36m'
 NC='\033[0m'
 
 echo ""
-echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____  ${NC}"
-echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \ ${NC}"
-echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) |${NC}"
-echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ < ${NC}"
-echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\\${NC}"
+echo -e "${LIGHT_CYAN}  ____ _   _    _    ____ _____ _____ ____   ${NC}"
+echo -e "${LIGHT_CYAN} / ___| | | |  / \  |  _ \_   _| ____|  _ \  ${NC}"
+echo -e "${LIGHT_CYAN}| |   | |_| | / _ \ | |_) || | |  _| | |_) | ${NC}"
+echo -e "${LIGHT_CYAN}| |___|  _  |/ ___ \|  _ < | | | |___|  _ <  ${NC}"
+echo -e "${LIGHT_CYAN} \____|_| |_/_/   \_\_| \_\|_| |_____|_| \_\ ${NC}"
 echo ""
 echo -e "${BOLD}${RED}Failed to generate Laravel application: {{ name }}${NC}"
 echo ""
