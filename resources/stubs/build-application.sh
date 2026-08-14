276f22b8c14cc875ca2b35c2a559dd61ff881799
Wilsen Hernández <wilsenh95@gmail.com>
2026-08-14 10:42:23 -0400

    feat: install gd extension

--- resources/stubs/build-application.sh
+++ resources/stubs/build-application.sh
@@ -42,6 +42,7 @@ docker run --rm \
 @if($isCustomStarterKit)
         export PATH=/usr/local/node/bin:\$PATH && \
 @endif
+        install-php-extensions gd >/dev/null 2>&1 && \
         docker-php-serversideup-dep-install-debian git >/dev/null 2>&1 && \
         composer global require laravel/installer --no-interaction --no-progress && \
         php \$(composer config --global home)/vendor/bin/laravel new {!! $name !!} {!! $options !!} --no-interaction ; \
